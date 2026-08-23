5cdbe82e999c5adb2cbf974ae65cf43b6a332033
expert-zealous <sh4ntoz@gmail.com>
2026-08-23 16:24:34 +0700

    uno-matematika-v11

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'uno-matematika-v10';
+const CACHE_NAME = 'uno-matematika-v11';
 const ASSETS = ['./', './index.html', './manifest.json', './assets/logo-uno-matematic.png', './favicon.ico', './icon-192.png', './icon-512.png'];
 self.addEventListener('install', (event) => {
   event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)).then(() => self.skipWaiting()));
